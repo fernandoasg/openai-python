1aa7dc3fb832466e44ce3c6ac2e58ae76ebc9cb0
Fernando <your-noreply-email-address>
2026-01-27 20:57:03 -0300

    Analisador de sentimentos

---
+++ analisador_sentimentos.py
@@ -0,0 +1,16 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+modelo = "gpt-4"
+
+def carrega(nome_do_arquivo):
+    try:
+        with open(nome_do_arquivo, "r") as arquivo:
+            dados = arquivo.read()
+            return dados
+    except IOError as e:
+        print(f"Erro: {e}")
